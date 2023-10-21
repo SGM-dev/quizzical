c2b1719628931657c2a036dc00edc97b0de489e4
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-10-21 23:51:55 +0530

    Build Start component

---
+++ src/components/Start.jsx
@@ -0,0 +1,11 @@
+import "./Start.css";
+
+export default function Start() {
+  return (
+    <div className="start-container">
+        <h1 className="title">Quizzical</h1>
+        <p className="desc">Some description if needed</p>
+        <button className="start-quiz">Start quiz</button>
+    </div>
+  );
+}
