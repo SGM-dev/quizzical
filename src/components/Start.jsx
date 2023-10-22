f287214581df55c65a88b80a11e71af492964019
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-10-22 16:15:07 +0530

    begin quiz on click

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,11 +1,13 @@
 import "./Start.css";
 
-export default function Start() {
+export default function Start(props) {
   return (
     <div className="start-container">
-        <h1 className="title">Quizzical</h1>
-        <p className="desc">Some description if needed</p>
-        <button className="start-quiz">Start quiz</button>
+      <h1 className="title">Quizzical</h1>
+      <p className="desc">Some description if needed</p>
+      <button className="start-quiz" onClick={() => props.setQuiz(true)}>
+        Start quiz
+      </button>
     </div>
   );
 }
